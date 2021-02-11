07f54c25ce67d60fd5f480f08bbc50adf613723b
vedin-dewan <77207674+vedin-dewan@users.noreply.github.com>
2021-02-11 16:16:03 +0000

    Add files via upload

---
+++ Focal Spot Analysis/focal_spot_analy_gradient(fast)_v2.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 13:07:52 2021
+
+@author: maximillianstunt & vedindewan 
+"""
+"""
+This works out a gradient)both x and y dirn) to find out background
+Then finds fitness parameter for focal spot by squaring pixel values
+and normalising to total pixel value.
+"""
+
+import numpy as np
+from matplotlib import image
+import time
+import matplotlib.pyplot as plt
+
+def read_in_img(filename):
+    pixel_values = np.array(image.imread(filename))
+    y_max=len(pixel_values)#rows
+    x_max=len(pixel_values[0])#columns
+    pixel_values=np.delete(pixel_values,np.s_[y_max-20:y_max],0)
+    return pixel_values
+
+def corners_av(pixels,cornersize):
+    y_max=len(pixels)#rows
+    x_max=len(pixels[0])#columns
+    c=cornersize
+    t_l=np.average(pixels[0:c,0:c])
+    b_l=np.average(pixels[y_max-c:y_max,0:c])
+    b_r=np.average(pixels[y_max-c:y_max,x_max-c:x_max])
+    t_r=np.average(pixels[0:c,x_max-c:x_max])
+    return np.array([t_l,t_r,b_r,b_l])
+
+def grad(corners,pixels):
+    y_max=len(pixels)#rows
+    x_max=len(pixels[0])#columns
+    Upper_grad=(corners[1]-corners[0])/x_max
+    left_grad=(corners[3]-corners[0])/x_max
+    bottom_grad=(corners[1]-corners[2])/y_max
+    right_grad=(corners[3]-corners[2])/y_max
+    return np.array([Upper_grad,left_grad,bottom_grad,right_grad])
+
+
+def read(filename,cornersize):
+    
+    start=time.time()
+    pixels=read_in_img(filename)
+    y_max=len(pixels)#rows
+    x_max=len(pixels[0])#columns
+    '''
+    This is needed as imread will not do maths properly on any pixel 
+    image reading will now output focal spot with background corrected
+    and a metric that correlates to shape of focal spot only
+    '''
+    pixel_true=np.zeros((len(pixels),len(pixels[0])))#NEEDED AS THIS WILL NOT BE ABLE TO HANDLE MATHS OTHERWISE
+    pixel_true[0:len(pixels),0:len(pixels[0])]=pixels[0:len(pixels),0:len(pixels[0])] #NEEDED 
+    
+
+    
+    corners=corners_av(pixels,cornersize)
+    grads=grad(corners,pixels)
+    pixel_background=np.zeros((y_max,x_max))
+   
+    pixels_coord=np.mgrid[0:y_max,0:x_max].reshape(2,-1).T
+    
+   
+    
+    pixel_background[0:y_max,0:x_max]=(pixels_coord[0:len(pixels_coord),0]*grads[0]+pixels_coord[0:len(pixels_coord),1]*grads[1])+corners[0] 
+    pixel_true=np.round(pixel_true-pixel_background)
+   
+    total_pixels=np.sum(pixels)
+   
+   
+    fitness_parameter=np.sum(pixel_true**2)/(total_pixels)
+    
+    end=time.time()
+    print(end-start)
+    print(fitness_parameter)
+    #plt.imshow(pixel_true)
+    return fitness_parameter
+
+read('/Users/vedindewan/Desktop/MSci_Project_AO/Focal_Spot_Analysis/Test_Spot_Data/0_0.jpg',6)    
